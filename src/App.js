a61d18db0192d8bf1961627c00bc0a392cbe4086
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2018-01-15 14:54:02 -0800

    goes through release 1 of react workshop

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-//import BasicComponent from './components/BasicComponent'
+import BasicComponent from './components/BasicComponent'
+import MyFirstComponent from './components/MyFirstComponent'
 
 class App extends Component {
   render() {
@@ -17,6 +18,10 @@ class App extends Component {
         <div className="App-intro">
           This is the starting point to build your app on top of!
         </div>
+
+        <BasicComponent />
+
+        <MyFirstComponent />
 
       </div>
     );
--- src/components/MyFirstComponent.js
+++ src/components/MyFirstComponent.js
@@ -1,7 +1,31 @@
 import React, {Component} from 'react'
 
 class MyFirstComponent extends Component {
-
+  render(){
+    return(
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Discription</th>
+            <th>Price</th>
+          </tr>
+          <tr>
+            <td>Cat</td>
+            <td>A fuzzy ball of fun</td>
+            <td>$100/hr</td>
+          </tr>
+          <tr>
+            <td>Dog</td>
+            <td>More work, but greater payoff to keep</td>
+            <td>$120/hr</td>
+          </tr>
+          <tr>
+            <td>Turtle</td>
+            <td>Low maintenance but still loving</td>
+            <td>$15/hr</td>
+          </tr>
+        </table>)
+  }
 }
 
 export default MyFirstComponent;
